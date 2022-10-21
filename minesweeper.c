f4421c14d9f971b7ec4065103aeb038ab62532e4
maf <61792345+mafgit@users.noreply.github.com>
2022-10-21 12:32:35 +0500

    Update minesweeper.c

--- minesweeper.c
+++ minesweeper.c
@@ -6,15 +6,15 @@
 void printInstructions()
 {
     printf("\nFollowing commands are available:\n");
-    printf("\n1) 'f <row> <column>' to flag a box e.g. f 1 2\n");
-    printf("\n2) 'u <row> <column>' to unflag a box e.g. u 1 2\n");
-    printf("\n3) 'c <row> <column>' to click a box e.g. c 1 2\n");
+    printf("\n1) 'f' to flag a box\n");
+    printf("\n2) 'c' to click a box\n");
+    printf("\n3) 'u' to unflag a box\n");
     printf("\nNote: Rows and columns must not be outside the range of [0, size - 1]\n");
-    // using the format f<row><col> without spaces resulted in waiting for more input apparently.
 }
 
 int main()
 {
+	srand(time(0));
     int size = 5;                        // ask for size e.g. 5 means 5x5
     int mines = (size * (size - 1)) / 2; // make levels of difficulty
     char grid[size][size];
@@ -29,25 +29,26 @@ int main()
     printInstructions();
 
     // === setting grid ===
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
+    int i, j;
+    for (i = 0; i < size; i++)
+        for (j = 0; j < size; j++)
             grid[i][j] = '*'; // initially star only
 
     // print 'mines left' and grid
-    printf("\n\nMines left: %d", mines);
+    printf("\n\nMines Left: %d", mines);
     // printing column numbers
     printf("\n\n   "); // two lines gap and then some space for row numbers
-    for (int i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
     {
         printf("%d  ", i);
     }
     printf("\n\n");
 
     // printing row numbers and then the rows
-    for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
     {
         printf("%d  ", i);
-        for (int j = 0; j < size; j++)
+        for (j = 0; j < size; j++)
             if (grid[i][j] == 'm') // if its a mine then print *, i.e. don't show it
                 printf("%c  ", '*');
             else
@@ -59,21 +60,27 @@ int main()
 
     do
     {
-
-        // user enters command in the form of e.g. f 2 4
-        printf("\nEnter command: ");
-        scanf("%c %d %d", &cmd, &thisRow, &thisCol);
+        printf("\nEnter Command: ");
+        scanf(" %c", &cmd);
 
         if (cmd == 'f' || cmd == 'c' || cmd == 'u') // if cmd is other than these then err
         {
+        	printf("\nEnter Coordinates in the Form <row> <col>: ");
+        	scanf("%d %d", &thisRow, &thisCol);
             if (thisRow >= 0 && thisCol >= 0 && thisRow < size && thisCol < size) // row and col no. must be in range
             {
                 // think more about what to do on each cmd and what if game hasn't started yet
                 if (!started)
                 {
                     // set up mines
-                    grid[0][0] = 'm';
-                    started = 1;
+                    int noOfMines = rand() % (((size * size) / 2) - 4) + 5; // 5 to half of size * size e.g. 5 to 12 in a 5x5 grid
+                    for (i = 0; i < noOfMines; i++) {
+                    	int randX, randY;
+						while (randX == thisRow && randY == thisCol) // to prevent from the first box touched being the mine
+                    		randX = rand() % size, randY = rand() % size;
+                    	grid[randX][randY] = 'm';
+					}
+					started = 1;
                 }
 
                 if (cmd == 'c') // user clicked on box
@@ -86,15 +93,19 @@ int main()
                 }
             }
             else
-                printf("Invalid row or column");
+                printf("\nInvalid Row or Column");
         }
+        else if (cmd == 'e') {
+        	printf("\nGame Ended");
+        	break;
+		}
         else
-            printf("Invalid command");
+            printf("\nInvalid Command");
 
     } while (!win && !blast); // enter conditions
 
     if (win == 1)
-        printf("You won");
+        printf("You Won");
     else if (blast == 1)
-        printf("You failed");
+        printf("You Failed");
 }
